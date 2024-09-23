62ddac23990a9bd8303553f69c9da4cfed29052a
Sterlingh78 <sterlingh78@yahoo.com>
2024-09-23 14:18:20 -0600

    Auth Done

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,7 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
-
 export default async function Index() {
-  return (
-    <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
-    </>
-  );
+	return (
+		<div className='flex items-center justify-center h-screen w-screen'>
+			<h1 className='m-auto'>Login to Get Started</h1>
+		</div>
+	);
 }
